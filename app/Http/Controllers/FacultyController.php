59cc7dfd40dd1991ac649b948fc791a5c7348f08
Urbaeva <burulaiurbaeva7@gmail.com>
2024-04-19 23:23:06 +0600

    added post, faculty controller

--- app/Http/Controllers/FacultyController.php
+++ app/Http/Controllers/FacultyController.php
@@ -2,9 +2,49 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\Faculty\StoreRequest;
+use App\Http\Requests\Faculty\UpdateRequest;
+use App\Models\Faculty;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 
 class FacultyController extends Controller
 {
-    //
+    public function index(): Factory|Application|View
+    {
+        $faculties = Faculty::all();
+        return view('faculty.index', compact('faculties'));
+    }
+
+    public function create(): Factory|Application|View
+    {
+        return view('faculty.create');
+    }
+
+    public function store(StoreRequest $request): RedirectResponse
+    {
+        $data = $request->validated();
+        Faculty::create($data);
+        return redirect()->route('faculty.index');
+    }
+
+    public function edit(): Factory|\Illuminate\Foundation\Application|View|Application
+    {
+        return view('faculty.edit');
+    }
+
+    public function update(UpdateRequest $request, Faculty $faculty): RedirectResponse
+    {
+        $data = $request->validated();
+        $faculty->update($data);
+        return redirect()->route('faculty.index');
+    }
+
+    public function delete(Faculty $faculty): RedirectResponse
+    {
+        $faculty->delete();
+        return redirect()->route('faculty.index');
+    }
 }
